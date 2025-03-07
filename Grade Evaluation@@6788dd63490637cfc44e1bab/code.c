f9923cdb01327bfde97d55e77bf841a5da89878d
gurleensingh06 <gurleen0300.becse24@chitkara.edu.in>
2025-03-07 15:12:52 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    char a[5];
-    scanf("%s",&a);
-    if(a=="A"){
+    char a;
+    scanf("%c",&a);
+    if(a=='A'){
         printf("Excellent");
     }   
-    else if(a=="B"){
+    else if(a=='B'){
         printf("Good");
     }
-    else if(a=="C"){
+    else if(a=='C'){
         printf("Average");
     }
-    else if(a=="D"){
+    else if(a=='D'){
         printf("Below Average");
     }
     else{
