75c60e5ce4b0ea563528fad1038c11d962d0f490
gurleensingh06 <gurleen0300.becse24@chitkara.edu.in>
2025-03-07 15:11:00 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -2,17 +2,17 @@
 
 int main(){
     char a[5];
-    scanf("%s",&a);
-    if(a=='A'){
+    scanf("%",&a);
+    if(a=="A"){
         printf("Excellent");
     }   
-    else if(a=='B'){
+    else if(a=="B"){
         printf("Good");
     }
-    else if(a=='C'){
+    else if(a=="C"){
         printf("Average");
     }
-    else if(a=='D'){
+    else if(a=="D"){
         printf("Below Average");
     }
     else{
